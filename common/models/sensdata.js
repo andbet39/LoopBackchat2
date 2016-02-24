d829f91d63b4defc665ac83825fa0e5e31a05efd
Andrea Terzani <andrea.terzani@gmail.com>
2016-02-24 07:37:11 +0100

    first

--- common/models/sensdata.js
+++ common/models/sensdata.js
@@ -1,10 +1,11 @@
 module.exports = function(Sensdata) {
 
+
     Sensdata.observe('after save', function(ctx, next) {
         if (ctx.instance) {
-           
+
           Sensdata.app.io.to(ctx.instance.sens_id).emit('senordata_created',ctx.instance);
-          
+
         } else {
           console.log('Updated %s matching %j',
             ctx.Model.pluralModelName,
@@ -12,5 +13,68 @@ module.exports = function(Sensdata) {
         }
         next();
     });
-  
+
+    Sensdata.gethourStats = function(sens_id,start_dt,end_dt, cb) {
+      Sensdata.getDataSource().connector.connect(function(err, db) {
+        var collection = db.collection('Sensdata');
+
+        console.log(start_dt);
+        collection.aggregate([
+          { $match: { sens_id: sens_id,
+                      $and:[
+                        {received:{$gte:start_dt}},
+                        {received:{$lte:end_dt}}
+                      ]
+                    }
+          },
+          { $group: {"_id": {
+            "year" : {
+              $year : "$received"
+            },
+            "dayOfYear" : {
+              $dayOfYear : "$received"
+            },
+            "hour" : {
+              $hour : "$received"
+            }
+          },
+            "count": {
+              $sum: 1
+            },
+            "avg": {
+              $avg: "$val"
+            },
+            "min": {
+              $min: "$val"
+            },
+            "max": {
+              $max: "$val"
+            }
+            }},{
+            $sort: {
+              "_id.year" : 1,
+              "_id.dayOfYear" : 1,
+              "_id.hour" : 1
+            }
+          }
+        ], function(err, data) {
+          if (err) return cb(err);
+          return cb(null, data);
+        });
+      });
+   };
+
+
+    Sensdata.remoteMethod (
+      'gethourStats',
+      {
+        http: {path: '/gethourstats', verb: 'get'},
+        accepts: [{arg: 'sens_id', type: 'number', http: { source: 'query' } },
+                 {arg: 'start_dt', type: 'date', http: { source: 'query' } },
+                 {arg: 'end_dt', type: 'date', http: { source: 'query' } }
+        ],
+
+        returns: {arg: 'stats', type: 'string'}
+      }
+    );
 };
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 
 var redis = require("redis"),
-    redis_client = redis.createClient();
+    redis_client = redis.createClient(6379,'10.0.0.1');
 
 
 
@@ -29,16 +29,16 @@ boot(app, __dirname, function(err) {
   // start the server if `$ node server.js`
   if (require.main === module){
     //app.start();
-    
-     
-    
+
+
+
       redis_client.on("message", function (channel, message) {
       //console.log("Nodejs : "+ message);
-      
+
       var res = message.split(":");
-      
+
       if(res[0]=="SN"){
-        
+
         var Sensdata = app.models.Sensdata;
           Sensdata.create({
             sens_id:res[1],
@@ -46,19 +46,19 @@ boot(app, __dirname, function(err) {
             type:res[3].replace(/\r?\n|\r/g, ""),
             received:new Date()
           });
-        
-      }
-      
-    });
-    
-    
-    redis_client.subscribe("messages");
 
-    
+      }
+
+    });
+
+
+    //redis_client.subscribe("messages");
+
+
     app.io = require('socket.io')(app.start(),{origins:'localhost:*'});
     app.redis_client =redis_client;
-    
-    
+
+
     app.io.on('connection', function(socket){
       console.log('a user connected');
 
@@ -67,13 +67,13 @@ boot(app, __dirname, function(err) {
           console.log('joining room', room);
           socket.join(room);
       });
-      
+
       socket.on('leave', function(room) {
           console.log('Leaving room'+room);
           socket.leave(room);
       });
-      
-     
+
+
 
       socket.on('message',function(message){
         //console.log(message);
